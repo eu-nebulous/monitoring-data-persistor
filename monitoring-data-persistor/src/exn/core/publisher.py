2634366b0f08aad07e5656bc2afe546e99218e91
atsag <andreastsagkaropoulos@hotmail.com>
2025-04-09 13:21:48 +0300

    Improvements in the handling of the storage of predicted metrics and exn library logging
    Change-Id: I2e94e2c54cb5f2063109f076f170168e5f6781f0

--- monitoring-data-persistor/src/exn/core/publisher.py
+++ monitoring-data-persistor/src/exn/core/publisher.py
@@ -1,12 +1,13 @@
 import datetime
 import logging
+from logging import WARNING
 
 from proton import Message
 
 from . import link
 
 _logger = logging.getLogger(__name__)
-
+_logger.setLevel(WARNING)
 
 class Publisher(link.Link):
 
